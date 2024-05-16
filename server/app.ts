4858b7a8cd36c03109bbfbf2269127216f98244d
edwin onyango <ookoedwin29@gmail.com>
2024-05-16 19:12:07 +0300

    Added database and redis connections.

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,30 @@
-import express from "express"
-export const app =express()
+require("dotenv").config();
+import express, { Request, Response, NextFunction } from "express";
+export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//body parser
+app.use(express.json({ limit: "50mb" }));
+//cookie-parser
+app.use(cookieParser());
+//cors
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+//test api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "Test api is working",
+  });
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
