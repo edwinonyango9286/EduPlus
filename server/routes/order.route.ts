bbbe4346e9b06f847085684e16a4c853b5d61bc0
edwin onyango <ookoedwin29@gmail.com>
2024-09-10 07:43:51 +0300

    updates

--- server/routes/order.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { authorizationRoles, isAunthenticated } from "../middleware/auth";
-import { createOrder, getAllOrders } from "../controllers/order.controller";
-const orderRoute = express.Router();
-
-orderRoute.post("/create-order", isAunthenticated, createOrder);
-orderRoute.get("/get-all-orders", isAunthenticated,authorizationRoles("admin"), getAllOrders);
-
-export default orderRoute;
